72d807160618ca2957e006cee5ce68c5ee24fea3
Dylan Piercey <pierceydylan@gmail.com>
2017-02-10 11:24:02 -0800

    Add ability to pass compiler directly

--- load.js
+++ load.js
@@ -2,6 +2,8 @@
 
 var path = require('path')
 var webpack = require('webpack')
+var Compiler = webpack.Compiler
+var MultiCompiler = webpack.MultiCompiler
 var root = path.join(__dirname, '/../../../')
 var compilers = {}
 
@@ -11,7 +13,8 @@ var compilers = {}
  * @param {String} file - the webpack config file to load.
  */
 module.exports = function loadConfig (file) {
-  if (typeof file !== 'string') throw new TypeError('@rill/webpack: config path must be a string.')
+  if (file instanceof Compiler || file instanceof MultiCompiler) return file
+  if (typeof file !== 'string') throw new TypeError('@rill/webpack: config path must be a string or webpack compiler.')
   if (!path.isAbsolute(file)) file = path.join(root, file)
   compilers[file] = compilers[file] || webpack(require(file))
   return compilers[file]
